1c8fd060a885b8c4dbb40d231fe910750771273f
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-24 19:03:14 +0500

    Task2

---
+++ Tyuiu.KarpenkoAL.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarpenkoAL.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double multSeries = 1;
+            int k = startValue;
+            do 
+            {
+                multSeries = multSeries * Math.Pow(((value * 1.0) / k), 3);
+                k++;
+            }
+            while (k <= stopValue);
+            return Math.Round(multSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint3.Task2.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KarpenkoAL.Sprint3.Task2.V14.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла do-while                                           *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #14                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет произве *");
+Console.WriteLine("* дение ряда пo формуле, при х=5                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int value = 5;
+int startValue = 1;
+int stopValue = 6;
+
+Console.WriteLine("Переменная Х = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+Console.ReadKey();
