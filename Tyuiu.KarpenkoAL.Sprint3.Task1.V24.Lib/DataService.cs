5166adf90ddeaf1d4447dce58a109322dfa3e03d
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-24 17:59:56 +0500

    Task1

---
+++ Tyuiu.KarpenkoAL.Sprint3.Task1.V24.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarpenkoAL.Sprint3.Task1.V24.Lib
+{
+    public class DataService : ISprint3Task1V24
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int i = startValue;
+            while (i <= stopValue)
+            {
+                sumSeries = sumSeries + (Math.Pow((2.0 / (6.0 + Math.Pow(value, i))), i));
+                i++;
+            }
+
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint3.Task1.V24/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KarpenkoAL.Sprint3.Task1.V24.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла while                                              *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #24                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда п *");
+Console.WriteLine("* o формуле, при х=5                                                      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int value = 5;
+int startValue = 1;
+int stopValue = 10;
+
+Console.WriteLine("Переменная Х = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+
+Console.ReadKey();
