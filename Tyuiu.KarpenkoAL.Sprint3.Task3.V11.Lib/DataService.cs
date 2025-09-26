9a071c52bda60e9a067ccb250a50d2ca79d165f4
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-26 05:53:48 +0500

    Task3

---
+++ Tyuiu.KarpenkoAL.Sprint3.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KarpenkoAL.Sprint3.Task3.V11.Lib
+{
+    public class DataService : ISprint3Task3V11
+    {
+        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
+        {
+            foreach( char chr in value) 
+            {
+                if (chr == replaceable) 
+                {
+                    value = value.Replace(chr, replacement);
+                }
+            }
+            return value;
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint3.Task3.V11/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.KarpenkoAL.Sprint3.Task3.V11.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #11                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу: используя цикл foreach заменить буквы q на цифру 7  *");
+Console.WriteLine("* в строке: sqwreq vqwqq q                                                *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string value = "sqwreq vqwqq q";
+char replaceable = 'q';
+char replacement = '7';
+
+Console.WriteLine("Исходная строка = " + value);
+Console.WriteLine("Искомый символ = " + replaceable); 
+Console.WriteLine("Новый символ = " + replacement);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Новая строка = " + ds.ReplaceCharOnNum(value, replaceable, replacement));
+
+Console.ReadKey();
+
