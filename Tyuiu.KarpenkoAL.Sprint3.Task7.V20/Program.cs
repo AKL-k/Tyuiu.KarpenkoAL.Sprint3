3906bed4c58dd6968711bc4094d6d0ae3ef09104
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-28 21:10:01 +0500

    Task7

---
+++ Tyuiu.KarpenkoAL.Sprint3.Task7.V20/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KarpenkoAL.Sprint3.Task7.V20.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #20                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Напишите программу, выводит таблицу значений функции на заданном диапоз *");
+Console.WriteLine("* оне [-5;5] с шагом 1. Произвести проверку деления на 0. При делении на  *");
+Console.WriteLine("* 0 вернуть значение 0. Значения округлить до двух знаков после запятой.  *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+double[] valueArray;
+valueArray = new double[len];
+
+valueArray = ds.GetMassFunction(startValue, stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("+----------+-----------+");
+Console.WriteLine("|    X     |   f(x)    |");
+Console.WriteLine("+----------+-----------+");
+for (int i = 0; i < len; i++) 
+{
+    Console.WriteLine("|{0,5:d}     |  {1, 6:f2}   |", startValue, valueArray[i]);
+    startValue++;
+}
+
+Console.WriteLine("+----------+-----------+");
+Console.ReadKey();
